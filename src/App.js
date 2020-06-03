9b921ef336d093c4d6a0a50181a822c85dde90df
venegasgabby76 <gabbyvenegas76@yahoo.com>
2020-06-03 16:42:30 -0700

    got started on the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,31 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+// import ReactDOM from 'react-dom';
+
+
+
+const todo = [
+  {
+    task: 'Do Laundry',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Clean Room',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Grocery store',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Workout',
+    id: Date.now(),
+    completed: false
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -7,7 +34,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Gabbys Todo List</h2>
+        <p> I AM PUTTING THIS HERE TO TEST THAT THIS IS WORKING</p>
+        <TodoList />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+const TodoList = props => {
+    return(
+        <div className='todo-list'>
+            <button>Click Here</button>
+        </div>
+    )
+}
+
+
+
+export default TodoList;
